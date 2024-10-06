9f41edd86ba5e6cf12c2659a0b58e765178d0afc
TurboG0 <sabadzodzuashvili2002@gmail.com>
2024-10-06 17:47:19 +0400

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/TurboG0/puppy
 
 go 1.21.1
+
+require github.com/TurboG0/dog v0.0.0-20241006134305-7e17924be9bd
--- puppy.go
+++ puppy.go
@@ -1,9 +1,17 @@
 package puppy
 
+import (
+	"github.com/TurboG0/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof!"
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
